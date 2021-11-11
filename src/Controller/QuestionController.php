43ea2f1b2d0e843b77ec9d3163ea7d023e70fafc
leeandri <rl.tsirih@gmail.com>
2021-11-11 05:24:42 +0300

    03-route-controller

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController
+{
+    public function homepage()
+    {
+        return new Response('What a bewitching controller we have conjured!');
+     }
+}
