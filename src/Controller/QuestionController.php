0f85cfbc4b55ae558ee27bacaf73c29bfa94081e
leeandri <rl.tsirih@gmail.com>
2021-11-11 05:53:05 +0300

    04-annotations

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
+     }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show a question "%s"!',
+            ucwords(str_replace('-', ' ',$slug))
+        ));
      }
 }
